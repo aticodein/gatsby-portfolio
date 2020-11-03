d53a8511ca066b0751f97e385a704618e325dbfc
Attila Szaloki <szalokiattila@gmail.com>
2020-11-03 17:47:27 +0000

    netlify deployment

---
+++ gatsby-simplefolio/src/mock/data.js
@@ -0,0 +1,94 @@
+import { nanoid } from 'nanoid';
+
+// HEAD DATA
+export const headData = {
+  title: '', // e.g: 'Name | Developer'
+  lang: '', // e.g: en, es, fr, jp
+  description: '', // e.g: Welcome to my website
+};
+
+// HERO DATA
+export const heroData = {
+  title: 'Hi, my name is',
+  name: 'Attila Szaloki',
+  subtitle: 'Full-stack Developer',
+  cta: 'Show more',
+};
+
+// ABOUT DATA
+export const aboutData = {
+  img: 'profile.jpg',
+  paragraphOne: '',
+  paragraphTwo: '',
+  paragraphThree: '',
+  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+};
+
+// PROJECTS DATA
+export const projectsData = [
+  {
+    id: nanoid(),
+    img: 'project.jpg',
+    title: '',
+    info: '',
+    info2: '',
+    url: '',
+    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: 'project.jpg',
+    title: '',
+    info: '',
+    info2: '',
+    url: '',
+    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: 'project.jpg',
+    title: '',
+    info: '',
+    info2: '',
+    url: '',
+    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+  },
+];
+
+// CONTACT DATA
+export const contactData = {
+  cta: '',
+  btn: '',
+  email: '',
+};
+
+// FOOTER DATA
+export const footerData = {
+  networks: [
+    {
+      id: nanoid(),
+      name: 'facebook',
+      url: '',
+    },
+    {
+      id: nanoid(),
+      name: 'codepen',
+      url: '',
+    },
+    {
+      id: nanoid(),
+      name: 'linkedin',
+      url: '',
+    },
+    {
+      id: nanoid(),
+      name: 'github',
+      url: 'https://github.com/aticodein',
+    },
+  ],
+};
+
+// Github start/fork buttons
+export const githubButtons = {
+  isEnabled: true, // set to false to disable the GitHub stars/fork buttons
+};
